687574ec22f96a7c12944a7beecdc9e365e60f5a
gs.song <rhrnrkd135@gmail.com>
2023-05-24 15:41:00 +0900

    [ + ] card ui 제작중

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hello Vue3 TV"/>
+  <CardUi :cardData="{...cardData, type:1}" /> 
+  <CardUi :cardData="{...cardData, type:2}" /> 
+  <CardUi :cardData="{...cardData, type:3}" /> 
+  <CardUi :cardData="{...cardData, type:4}" /> 
 </template>
 
 <script>
+import CardUi from '@/components/CardUi.vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    CardUi,
+  },
+  data() {
+    return {
+      cardData :{
+        type: Number,
+        title:'card',
+        description: '미쳣다 컴포넌트',
+      }
+    }
+  },
 }
 </script>
 
 <style>
+* {
+  margin:0;
+  padding:0;
+  user-select: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/CardUi.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="card-ui" :class="{ row : cardType === 4 }">
+    <figure>
+      <img src="../assets/img/logo.webp" />
+    </figure>
+    <div class="card-ui-textarea">
+      <h2 class="card-ui-title">{{ cardData.title }}</h2>
+      <p class="card-ui-description">{{ cardData.description }}</p>
+      <p class="card-ui-highlight"> {{ cardData }}</p>
+    </div>
+    <ul class="card-ui-review"> 
+      <li class="active"></li>
+      <li class="active"></li>
+      <li class="active"></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{
+      cardData: Object,
+    },
+    data() {
+      return {
+        cardType: Number,
+      }
+    },
+    created() {
+      this.cardType = this.cardData.type;
+    },  
+  }
+</script>
+
+<style scoped>
+  .card-ui {
+    display:inline-block;
+    width: 23%;
+    margin: 0 10px;
+    border: 1px solid #ccc;
+    min-height: 100px;
+    background-color: #f1f1f1;
+  }
+  .card-ui.row {
+    width: 50%;
+    margin-top: 50px;
+    display:flex;
+  }
+  .card-ui.row .card-ui-review {
+    display: none;
+  }
+  .card-ui figure img {
+    width: 100%;
+  }
+
+  .card-ui-textarea {
+    text-align: left;
+    box-sizing: border-box;
+    padding-left: 10px;
+  }
+  .card-ui-review {
+    border-top: 1px solid #111;
+    display: flex;
+    margin: 10px 0;
+    padding: 15px 0 15px 10px;
+    gap: 5px;
+    list-style: none;
+  }
+  .card-ui-review li{
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #c8c8c8;
+  }
+  .card-ui-review li.active {
+    background-color: orange;
+  }
+
+  
+</style>
